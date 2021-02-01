61e517d9026198497591dc66669a7ae9dc50bd7f
Garrett <gman9mm@live.com>
2021-02-01 11:17:59 -0800

    Build file structure and begin adding code.
    Co-authored-by: David Sterry <sterry.david@gmail.com>

---
+++ __tests__/coin.test.js
@@ -0,0 +1,19 @@
+// import Coin from '../src/js/coin.js';
+
+// describe('Coin', () => {
+
+// let coin;
+
+//   beforeEach(() => {
+//     rectangle = new Rectangle(3,5);
+//   });
+
+  // test('should create instance of coin', () => {
+  //   expect(coin.).toEqual(3);
+  //   expect(rectangle.side2).toEqual(5);
+  // });
+
+//   test('should correctly create a rectangle object using two sides', () => {
+//     expect(rectangle.getArea()).toEqual(15);
+//   });
+// });
---
+++ src/js/coin.js
@@ -0,0 +1,33 @@
+const quarter = .25;
+const dime = .10;
+const nickel = .05;
+const penny = .01;
+
+
+export default class Coin {
+
+  const = coinCounter = (counter) => {
+    if (NaN(counter)) {
+      return;
+    } else {
+      if (counter >= 5.00) {
+        return counter;
+      } else {
+
+      }
+    }
+  }
+
+
+// const incrementCounter = (counter) => {
+//     // This is the termination condition.
+//     if (isNaN(counter)) {
+//       return;
+//     }
+//     if (counter >= 3) {
+//       return counter;
+//     } else {
+//       console.log(counter);
+//       return incrementCounter(counter + 1);
+//     }
+//   }
