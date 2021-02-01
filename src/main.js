9984fa4a2a62d22f57210f3f4706936a5604f386
Garrett <gman9mm@live.com>
2021-02-01 15:26:08 -0800

    Almost functional redux.
    Co-authored-by: David Sterry <sterry.david@gmail.com>

--- src/main.js
+++ src/main.js
@@ -1,27 +1,38 @@
-// import $ from 'jquery';
-// import 'bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import './css/styles.css';
-// import Triangle from './js/triangle.js';
-// import Rectangle from './js/rectangle.js';
+// const quarter = .25;
+// const dime = .10;
+// const nickel = .05;
+// const penny = .01;
 
-// $(document).ready(function() {
-//   $('#triangle-checker-form').submit(function(event) {
-//     event.preventDefault();
-//     const length1 = parseInt($('#length1').val());
-//     const length2 = parseInt($('#length2').val());
-//     const length3 = parseInt($('#length3').val());
-//     const triangle = new Triangle(length1, length2, length3);
-//     const response = triangle.checkType();
-//     $('#response').append(`<p>${response}</p>`);
-//   });
 
-//   $('#rectangle-area-form').submit(function(event) {
-//     event.preventDefault();
-//     const length1 = parseInt($('#rect-length1').val());
-//     const length2 = parseInt($('#rect-length2').val());
-//     const rectangle = new Rectangle(length1, length2);
-//     const response = rectangle.getArea();
-//     $('#response2').append(`<p> The area of the rectangle is ${response}.</p>`);
-//   });
-// });
+const coinCounter = (amount, obj = {}) => {
+  if (isNaN(amount)) {
+    return;
+  }
+  if (amount === 0) {
+    return obj;
+  } else {
+    if (amount / .25 >= 1) {
+      const quarters = Math.floor(amount / .25);
+      obj.quarters = quarters;
+      console.log(quarters);
+      return coinCounter((amount - quarters * .25).toFixed(2), {});
+    }
+    else if (amount / .10 >= 1) {
+      const dimes = Math.floor(amount / .10);
+      obj.dimes = dimes;
+      return coinCounter((amount - dimes * .10).toFixed(2), {});
+    }
+    else if (amount / .05 >= 1) {
+      const nickels = Math.floor(amount / .05);
+      obj.nickels = nickels;
+      return coinCounter((amount - nickels * .05).toFixed(2), {});
+    }
+    else if (amount / .01 >= 1) {
+      const pennies = Math.floor(amount / .01);
+      obj.pennies = pennies;
+      return coinCounter((amount - pennies * .01).toFixed(2), {});
+    }
+  }
+};
+
+coinCounter(1.72);
